c159c74990131fde40ac93885af6d3d0e2172732
Julian Gomez <julian1995ag@gmail.com>
2022-11-14 15:15:51 -0300

    avances en entrega

---
+++ app.js
@@ -0,0 +1,26 @@
+const express = require('express')
+const {Server: HttpServer} = require('http')
+
+
+const indexRouter = require('./src/routes/index');
+
+const errorHandler = require('./src/middlewares/errorHandler');
+
+require('dotenv').config()
+ 
+
+const app = express();
+
+const http = new HttpServer(app);
+
+
+app.use(express.json())
+app.use(express.urlencoded({extended: true}))
+
+app.use(express.static(__dirname + '/public'));
+
+app.use('/api', indexRouter);
+
+app.use(errorHandler);
+
+module.exports = http;
